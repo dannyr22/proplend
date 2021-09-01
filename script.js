2f789a5284b929ea4f5804e1d49b920c01a3a105
Danny Roberts <dannyroberts.b@gmail.com>
2021-09-01 01:31:38 +0100

    fleshed out the website but it still needs quite a bit of styling

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const imageAndTextSections = document.querySelectorAll(
+  ".image-and-text-container"
+);
+
 $(document).ready(function () {
   // $(".navigation-menu").hide();
+  // var $imageAndTextSections = $(".image-and-text-container");
+  // var $imageAndTextFirst = $imageAndTextSections[0];
+  // var $imageAndTextSecond = $imageAndTextSections[1];
+  // $imageAndTextFirst.find(".text-section").css("background-color", "black");
+
+  var firstSection = imageAndTextSections[0];
+  var secondSection = imageAndTextSections[1];
+  firstSection.children[1].style.backgroundColor = "rgb(249, 189, 78)";
+  secondSection.children[1].style.backgroundColor = "#777777";
+  secondSection.style.flexDirection = "row-reverse";
 });
